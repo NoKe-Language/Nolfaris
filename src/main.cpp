270bc51a2ca8534e711f4b5295bbc823aad05cf2
Martin Danhier <martin.danhier@student.unamur.be>
2019-09-27 18:31:18 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void test(void){
+    cout << "hello from another function";
+}
+
+int main(int argc, const char * argv[]) {
+    int bob = 0;
+        cout << bob++ << endl;
+
+
+        cout << endl;
+        system("pause");
+    return 0;
+}
+
